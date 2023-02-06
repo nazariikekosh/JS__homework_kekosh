9a401de69c9804de4595f8370650e33246238b4c
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-02-06 20:07:38 +0200

    add__task_2

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,22 @@
 // -----Завдання_1-----
-const USER_NAME = prompt('Введіть Ваше імя:');
-if (USER_NAME.length == 0){
-    alert('Ви не вказали імя');
-    window.location.reload()
-} else {
-    alert(`Привіт, ${USER_NAME}!`);
-}
+// const USER_NAME = prompt('Введіть Ваше імя:');
+// if (USER_NAME.length == 0){
+//     alert('Ви не вказали імя');
+//     window.location.reload()
+// } else {
+//     alert(`Привіт, ${USER_NAME}!`);
+// }
 
 
 // -----Завдання_2-----
-// const YEAR = new Date().getFullYear();
-// const USER_AGE = Number(prompt('Введіть свій рік народження:'));
-// if (USER_AGE < 0 ){
-//     alert('Ви ввели невірне значення');
-//     window.location.reload()
-// } else if (){
-//     alert(`Ваш вік: ${YEAR - USER_AGE}`);
-// }
+const YEAR = new Date().getFullYear();
+const USER_AGE = prompt('Введіть свій рік народження:');
+if (USER_AGE < 0 ){
+    alert('Ви ввели невірне значення');
+    window.location.reload()
+} else if (USER_AGE.length == 0){
+    alert('Ви не вказали свій рік народження!');
+    window.location.reload()
+} else {
+    alert (`Ваш вік: ${YEAR - USER_AGE}`);
+}
