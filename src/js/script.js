227960653fbeed22dfafedc84aa1d816b400094e
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-02-08 12:57:32 +0200

    update_

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,27 @@
-// Завдання 1
-let name = prompt("Введіть Ваше імя:");
-alert(`Привіт, ${name}!`);
+//Завдання 1
+const USER_NAME = prompt('Введіть Ваше імя:');
+if (USER_NAME.length == 0){
+    alert('Ви не вказали імя');
+    window.location.reload()
+} else {
+    alert(`Привіт, ${USER_NAME}!`);
+}
+
 
 // Завдання 2
-const year = new Date().getFullYear();
-let userYear = prompt("Введіть Ваш рік народженнея:");
-
-if(userYear<0){
-    alert("ти шо гониш?)")
-}else{
-
-    alert(`Ваш вік: ${year - userYear} років`); 
+const YEAR = new Date().getFullYear();
+const USER_AGE = prompt('Введіть свій рік народження:');
+if (USER_AGE < 0 ){
+    alert('Ви ввели невірне значення');
+    window.location.reload()
+} else if (USER_AGE.length == 0){
+    alert('Ви не вказали свій рік народження!');
+    window.location.reload()
+} else {
+    alert (`Ваш вік: ${YEAR - USER_AGE}`);
 }
+
+
 
 // Завдання 3
 let sideOfSquare = prompt("Введіть сторону квадрата:");
@@ -64,15 +74,3 @@ if (number % 2 == 0) {
 } else if (number % 2 !=0) {
     alert(`Число не парне!`)
 }
-
-
-
-
-
-
-
-
-
-
-
-
